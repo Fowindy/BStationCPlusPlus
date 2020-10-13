7299465fa202196fc9494ff9d6074c46b3c7fbfe
Fowindy <47002339@qq.com>
2020-10-14 07:08:51 +0800

    02_注释

---
+++ Src/01_C++书写HelloWorld/02_注释.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//1.单行注释
+//2.多行注释
+/*
+	main是一个程序的入口
+	每个程序都必须有这么一个函数
+	有且仅有一个
+*/
+int main1()
+{
+	//7行代码的含义就是在屏幕输出Hello World
+	std::cout << "hello World" << std::endl;
+	system("pause");
+	return 0;
+}
