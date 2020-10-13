a4f71cb33ad6cf6901d62e1c16f8ae5960e1bc27
Fowindy <47002339@qq.com>
2020-10-14 07:33:54 +0800

    05_关键字

--- Src/01_C++书写HelloWorld/04_常量.cpp
+++ Src/01_C++书写HelloWorld/04_常量.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define Day 7
 //2.const修饰的变量
 const int month = 12;
-int main()
+int main4()
 {
 	//Day = 14;//错误:Day是常量,一旦修改就会报错
 	std::cout<<"一周共有:"<<Day<<"天"<<std::endl;
---
+++ Src/01_C++书写HelloWorld/05_关键字.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//创建变量:数据类型	变量名称 = 变量初始值
+	//不要用关键字给变量或者常量起名称
+	//int int = 10: 错误, 第三个int是关键字, 不可以作为变量的名称
+	system("pause");
+	return 0;
+}
