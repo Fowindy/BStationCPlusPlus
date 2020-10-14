386bae02726ccdc26c068feac39b0d131955099a
Fowindy <47002339@qq.com>
2020-10-14 15:21:39 +0800

    18_运算符-赋值运算符

---
+++ Src/01_C++书写HelloWorld/18_运算符-赋值运算符.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	//赋值运算符
+	// =
+	int a = 10;
+	a = 100;
+	std::cout << "a = " << a << std::endl;//100
+	// +=
+	a = 10;
+	a += 2;//等价于 a = a + 2;
+	std::cout << "a = " << a << std::endl;//12
+	// -=
+	a = 10;
+	a -= 2;//等价于 a = a -2;
+	std::cout << "a = " << a << std::endl;//8
+	// *=
+	a = 10;
+	a *= 2;//等价于 a = a * 2;
+	std::cout << "a = " << a << std::endl;//20
+	// /=
+	a = 10;
+	a /= 2;//等价于 a = a / 2;
+	std::cout << "a = " << a << std::endl;//5
+	// %=
+	a = 10;
+	a %= 2;//等价于 a = a % 2;
+	std::cout << "a = " << a << std::endl;//0
+	system("pause");
+	return 0;
+}
