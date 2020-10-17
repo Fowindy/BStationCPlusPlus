6b8062bba6514228a56e6d44fada1af9d857d1fa
Fowindy <47002339@qq.com>
2020-10-17 13:52:53 +0800

    26_程序流程控制-选择结构_嵌套if语句

--- Src/01_C++书写HelloWorld/26_程序流程控制-选择结构_嵌套if语句.cpp
+++ Src/01_C++书写HelloWorld/26_程序流程控制-选择结构_嵌套if语句.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main25()
+int main()
 {
 	//程序流程控制-选择结构_多条件的if语句
 	//输入一个考试分数, 如果大于600分, 视为考上一本大学, 在屏幕输出
@@ -15,21 +15,33 @@ int main25()
 	if (score > 600)
 	{
 		std::cout<<"恭喜你!考上一本大学"<<std::endl;
+		if (score > 700)
+		{
+			std::cout<<"你能考上清华大学"<<std::endl;
+		}
+		else if (score > 650)
+		{
+			std::cout<<"你能考上北京大学"<<std::endl;
+		}
+		else
+		{
+			std::cout<<"你能考上人民大学"<<std::endl;
+		}
 	}
 	//4、大于500, 视为考上二本大学, 屏幕输出
 	else if (score > 500)
 	{
-		std::cout<<"恭喜你!考上二本大学"<<std::endl;
+		std::cout << "恭喜你!考上二本大学" << std::endl;
 	}
 	//5、大于400, 视为考上三本大学, 屏幕输出
 	else if (score > 400)
 	{
-		std::cout<<"恭喜你!考上三本大学"<<std::endl;
+		std::cout << "恭喜你!考上三本大学" << std::endl;
 	}
 	//6、小于等于400分,视为未考上本科,屏幕上输出
 	else
 	{
-		std::cout<<"很遗憾!没有考上大学"<<std::endl;
+		std::cout << "很遗憾!没有考上大学" << std::endl;
 	}
 	system("pause");
 	return 0;
