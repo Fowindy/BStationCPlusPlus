bc2bd248b03c996c1b5a03a3b818d9d914c1d33d
Fowindy <47002339@qq.com>
2020-10-17 11:13:35 +0800

    22_运算符-逻辑运算符_或

--- Src/01_C++书写HelloWorld/22_运算符-逻辑运算符_或.cpp
+++ Src/01_C++书写HelloWorld/22_运算符-逻辑运算符_或.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main21()
+int main()
 {
-	// 逻辑运算符_与(&&)
+	// 逻辑运算符_或(||)
 	int a = 10;
 	int b = 10;
-	cout << (a && b) << endl;//1
+	cout << (a || b) << endl;//1
 
 	a = 0;
 	b = 10;
-	cout << (a && b) << endl;//0
+	cout << (a || b) << endl;//1
 
 	a = 0;
 	b = 0;
-	cout << (a && b) << endl;//0
+	cout << (a || b) << endl;//0
 
-	//同真为真,其余为假
+	//同假为假,其余为真
 	system("pause");
 	return 0;
 }
