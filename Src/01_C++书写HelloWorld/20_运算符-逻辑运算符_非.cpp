45ae815a3eebe93221d3343687bf0bb5cce1495e
Fowindy <47002339@qq.com>
2020-10-17 11:03:26 +0800

    20_运算符-逻辑运算符_非

---
+++ Src/01_C++书写HelloWorld/20_运算符-逻辑运算符_非.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	//逻辑运算符非!
+	int a = 10;
+	//在C++中除了0都为真
+	cout << !a << endl;//0
+	cout << !!a << endl;//1
+	//总结:真变假,假变真
+	system("pause");
+	return 0;
+}
