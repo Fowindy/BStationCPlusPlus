958e1cb834115f0fe8aaf63c8bd03075d1205d34
Fowindy <47002339@qq.com>
2020-10-17 18:10:41 +0800

    40_程序流程结构-跳转语句_goto语句

---
+++ Src/01_C++书写HelloWorld/40_程序流程结构-跳转语句_goto语句.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main40()
+{
+	std::cout << "1.xxxxxxx" << std::endl;
+	std::cout << "2.xxxxxxx" << std::endl;
+	goto FLAG;
+	std::cout << "3.xxxxxxx" << std::endl;
+	std::cout << "4.xxxxxxx" << std::endl;
+	FLAG:
+	std::cout << "5.xxxxxxx" << std::endl;
+	system("pause");
+	return 0;
+}
