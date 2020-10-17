de449d92d2e5a8b84784826bb24b4c7eb4063977
Fowindy <47002339@qq.com>
2020-10-17 16:51:56 +0800

    33_程序流程控制-循环结构案例_水仙花数

---
+++ Src/01_C++书写HelloWorld/33_程序流程控制-循环结构案例_水仙花数.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+int main()
+{
+	//案例描述:水仙花数是指一个3位数, 它的每个位上的数字的3次幕之和等于它本身
+	//例a : 1 ^ 3 + 5 ^ 3 + 3 ^ 3 = 153
+	//请利用do...while语句,求出所有3位数中的水仙花数
+	int num = 100;
+	do
+	{
+		if (pow(num / 100, 3) + pow(num % 100 / 10, 3) + pow(num % 10, 3) == num)
+		{
+			std::cout<<"水仙花数:"<<num<<std::endl;
+		}
+		num++;
+	} while (num < 1000);
+	system("pause");
+	return 0;
+}
