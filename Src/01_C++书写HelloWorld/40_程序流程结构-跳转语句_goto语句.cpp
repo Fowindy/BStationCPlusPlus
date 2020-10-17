ead710683f6eb9f09c7247c5d0cd7c2ed9151bde
Fowindy <47002339@qq.com>
2020-10-17 19:08:36 +0800

    41_数组-一维数组定义方式

--- Src/01_C++书写HelloWorld/40_程序流程结构-跳转语句_goto语句.cpp
+++ Src/01_C++书写HelloWorld/40_程序流程结构-跳转语句_goto语句.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main40()
+int main()
 {
 	std::cout << "1.xxxxxxx" << std::endl;
 	std::cout << "2.xxxxxxx" << std::endl;
---
+++ Src/02_数组/41_数组-一维数组定义方式.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	//数组
+	//1, 数据类型 数组名[数组长度];
+	//数组元素的下标是从0开始的
+	int arr[5];
+	arr[0] = 10;
+	arr[1] = 20;
+	arr[2] = 30;
+	arr[3] = 40;
+	arr[4] = 50;
+	//访问数组元素
+	std::cout<<arr[3]<<std::endl;//40
+	//2, 数据类型 数组名[数组长度]= (值1,值2 ..);
+	//如果在初始化数据的时候,没有完全填写完,会用0补充完整
+	int arr2[5] = { 10,20,30,40,50 };
+	//利用循环,输出数组中的元素
+	for (int i = 0; i < sizeof(arr2)/sizeof(arr2[0]); i++)
+	{
+		cout<<arr2[i]<<endl;
+	}
+	//3, 数据类型 数组名[] = (值1, 值2 ...);
+	int arr3[] = { 90,80,70,60,50,40,30,20,10 };
+	//利用循环,输出数组中的元素
+	for (int i = 0; i < sizeof(arr3) / sizeof(arr3[0]); i++)
+	{
+		cout << arr3[i] << endl;
+	}
+	system("pause");
+	return 0;
+}
