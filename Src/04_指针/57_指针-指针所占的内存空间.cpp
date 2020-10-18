970366dafcb390a913d689c091630c13caf646b5
Fowindy <47002339@qq.com>
2020-10-18 10:47:57 +0800

    57_指针-指针所占的内存空间

---
+++ Src/04_指针/57_指针-指针所占的内存空间.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	//指针所占的内存空间
+	int a = 10;
+	int *p = &a;
+	//在32位开发平台:任何类型的指针占用4个字节空间
+	//在64位开发平台:任何类型的指针占用8个字节空间
+	std::cout << "sizeof(p) = " << sizeof(p) << std::endl;//4
+	std::cout << "sizeof(int *) = " << sizeof(int *) << std::endl;//4
+	std::cout << "sizeof(double *) = " << sizeof(double *) << std::endl;//4
+	std::cout << "sizeof(float *) = " << sizeof(float *) << std::endl;//4
+	std::cout << "sizeof(char *) = " << sizeof(char *) << std::endl;//4
+	system("pause");
+	return 0;
+}
