292975a417c21958a703eebb913e5e78f896667e
Fowindy <47002339@qq.com>
2020-10-19 00:40:41 +0800

    通讯录管理系统_菜单界面完成

---
+++ Src/06_通讯录管理系统/通讯录管理系统.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//菜单界面
+void showMenu()
+{
+	cout << "***************************" << endl;
+	cout << "*****  1、添加联系人  *****" << endl;
+	cout << "*****  2、显示联系人  *****" << endl;
+	cout << "*****  3、删除联系人  *****" << endl;
+	cout << "*****  4、查找联系人  *****" << endl;
+	cout << "*****  5、修改联系人  *****" << endl;
+	cout << "*****  6、清空联系人  *****" << endl;
+	cout << "*****  0、退出通讯录  *****" << endl;
+	cout << "***************************" << endl;
+}
+int main()
+{
+	//菜单显示的调用
+	showMenu();
+	system("pause");
+	return 0;
+}
