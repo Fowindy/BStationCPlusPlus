1908d16db22a49c845bc8fefc53971fe51daa8e7
Fowindy <47002339@qq.com>
2020-10-19 00:54:09 +0800

    退出功能完成

--- Src/06_通讯录管理系统/通讯录管理系统.cpp
+++ Src/06_通讯录管理系统/通讯录管理系统.cpp
@@ -16,8 +16,38 @@ void showMenu()
 }
 int main()
 {
-	//菜单显示的调用
-	showMenu();
-	system("pause");
+	//创建用户选择变量
+	int select = 0;
+	while (true)
+	{
+		//菜单显示的调用
+		showMenu();
+		//提示用户输入选择
+		cout << "请输入选择(数字):";
+		std::cin >> select;
+		//用户选择控制
+		switch (select)
+		{
+		case 1://1、添加联系人
+			break;
+		case 2://2、显示联系人
+			break;
+		case 3://3、删除联系人
+			break;
+		case 4://4、查找联系人
+			break;
+		case 5://5、修改联系人
+			break;
+		case 6://6、清空联系人
+			break;
+		case 0://0、退出通讯录
+			cout << "退出系统,欢迎下次使用!" << endl;
+			system("pause");
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
 	return 0;
 }
