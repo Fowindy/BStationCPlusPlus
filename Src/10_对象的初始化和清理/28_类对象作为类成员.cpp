1207d26d31ed37ebe51533d848cf608e3acd259f
Fowindy <47002339@qq.com>
2020-10-21 19:58:41 +0800

    28_1.当其他类对象作为本类成员,构造时候先构造类对象，在构造自身，析构的顺序与构造相反

--- Src/10_对象的初始化和清理/28_类对象作为类成员.cpp
+++ Src/10_对象的初始化和清理/28_类对象作为类成员.cpp
@@ -8,7 +8,12 @@ class Phone
 public:
 	Phone(string phoneBrand)
 	{
+		cout<<"Phone的构造函数调用"<<endl;
 		m_PhoneBrand = phoneBrand;
+	}
+	~Phone()
+	{
+		cout<<"Phone的析构函数调用"<<endl;
 	}
 public:
 	//手机品牌名称
@@ -21,6 +26,11 @@ public:
 	//Phone m_Phone = phoneBrand;//隐式转换法
 	Person8(string name,string phoneBrand):m_Name(name),m_Phone(phoneBrand)
 	{
+		cout<<"Person8的构造函数调用"<<endl;
+	}
+	~Person8()
+	{
+		cout<<"Person8的析构函数调用"<<endl;
 	}
 public:
 	//姓名
@@ -28,10 +38,15 @@ public:
 	//手机类成员
 	Phone m_Phone;
 };
-int main()
+void test8()
 {
 	Person8 p("张三", "Apple");
-	cout<<p.m_Name<<"拿着:"<<p.m_Phone.m_PhoneBrand<<endl;
+	cout << p.m_Name << "拿着:" << p.m_Phone.m_PhoneBrand << endl;
+}
+//1.当其他类对象作为本类成员,构造时候先构造类对象，在构造自身，析构的顺序与构造相反
+int main()
+{
+	test8();
 	system("pause");
 	return 0;
 }
