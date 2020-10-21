2186ac124888efe09565850f6181331bcbc7074d
Fowindy <47002339@qq.com>
2020-10-21 19:35:58 +0800

    27_2.初始化列表初始化属性

--- Src/10_对象的初始化和清理/27_初始化列表.cpp
+++ Src/10_对象的初始化和清理/27_初始化列表.cpp
@@ -6,11 +6,16 @@ class Person7
 {
 public:
 	//1.传统初始化操作
-	Person7(int a,int b,int c)
+	//Person7(int a,int b,int c)
+	//{
+	//	m_A = a;
+	//	m_B = b;
+	//	m_C = c;
+	//}
+
+	//2.初始化列表初始化属性
+	Person7(int a,int b,int c) :m_A(a), m_B(b), m_C(c)
 	{
-		m_A = a;
-		m_B = b;
-		m_C = c;
 	}
 	int m_A;
 	int m_B;
@@ -18,7 +23,8 @@ public:
 };
 void test7()
 {
-	Person7 p(10, 20, 30);
+	//Person7 p(10, 20, 30);
+	Person7 p(15, 25, 35);
 	cout << "m_A = " << p.m_A << endl;
 	cout << "m_B = " << p.m_B << endl;
 	cout << "m_C = " << p.m_C << endl;
