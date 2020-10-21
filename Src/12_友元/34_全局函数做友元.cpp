a8be0ac057a02eec61bd7d25ca35144d34ade42f
Fowindy <47002339@qq.com>
2020-10-22 01:03:54 +0800

    35_类做友元

--- Src/12_友元/34_全局函数做友元.cpp
+++ Src/12_友元/34_全局函数做友元.cpp
@@ -2,12 +2,12 @@
 #include <string>
 using namespace std;
 //建筑物类
-class Building
+class Building4
 {
 	//全局函数友元的声明
-	friend void goodGay(Building* building);
+	friend void goodGay( Building4* building);
 public:
-	Building()
+	 Building4()
 	{
 		m_SittingRoom = "客厅";
 		m_BedRoom = "卧室";
@@ -18,14 +18,14 @@ private:
 	string m_BedRoom;//卧室
 };
 //全局函数
-void goodGay(Building* building)
+void goodGay( Building4* building)
 {
 	cout << "好基友全局函数正在访问:" << building->m_SittingRoom << endl;
 	cout << "好基友全局函数正在访问:" << building->m_BedRoom << endl;
 }
 void test4()
 {
-	Building building;
+	 Building4 building;
 	goodGay(&building);
 }
 int main34()
---
+++ Src/12_友元/35_类做友元.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//类做友元
+//声明类
+class Building5;
+//好基友类
+class GoodGay
+{
+public:
+	GoodGay();
+	//好基友类有参数函数的声明
+	void visit();
+	//好基友类中定义Building类成员
+	Building5* building;
+};
+//建筑物类
+class Building5
+{
+	//类做友元声明
+	friend class GoodGay;
+public:
+	//类内声明成员函数
+	Building5();
+public:
+	string m_SittingRoom;//客厅
+private:
+	string m_BedRoom;//卧室
+};
+//类外实现成员函数
+Building5::Building5()
+{
+	m_SittingRoom = "客厅";
+	m_BedRoom = "卧室";
+}
+GoodGay::GoodGay()
+{
+	//创建一个建筑物的对象
+	building = new Building5;
+}
+void GoodGay::visit()
+{
+	cout<<"好基友类正在访问:"<<building->m_SittingRoom<<endl;
+	cout<<"好基友类正在访问:"<<building->m_BedRoom<<endl;
+}
+void test5()
+{
+	GoodGay gg;
+	gg.visit();
+}
+int main()
+{
+	test5();
+	system("pause");
+	return 0;
+}
