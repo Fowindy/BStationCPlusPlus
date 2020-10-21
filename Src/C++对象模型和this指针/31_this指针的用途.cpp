a2ad88671f6dc59e7fdfef0e0a7b97bd8d9e1f8d
Fowindy <47002339@qq.com>
2020-10-21 21:49:37 +0800

    31_3.this是指向被调用的成员函数所属的对象p2的指针,而*this则是p2对象本体

--- Src/C++对象模型和this指针/31_this指针的用途.cpp
+++ Src/C++对象模型和this指针/31_this指针的用途.cpp
@@ -2,7 +2,6 @@
 #include <string>
 using namespace std;
 //this指针的用途
-//2.返回对象本身用*this
 class Person1
 {
 public:
@@ -10,6 +9,12 @@ public:
 	{
 		//this指针指向的被调用的成员函数所属的对象p1
 		this->age = age;
+	}
+	Person1& Person1AddAge(Person1 &p)
+	{
+		this->age += p.age;
+		//this是指向被调用的成员函数所属的对象p2的指针,而*this则是p2对象本体
+		return *this;
 	}
 	int age;
 };
@@ -19,9 +24,19 @@ void test1()
 	Person1 p1(18);
 	cout<<"p1的年龄为:"<<p1.age<<endl;
 }
+//2.返回对象本身用*this
+void test11()
+{
+	Person1 p1(10);
+	Person1 p2(10);
+	//链式编程思想
+	p2.Person1AddAge(p1).Person1AddAge(p1).Person1AddAge(p1);
+	cout<<"p2的年龄为:"<<p2.age<<endl;//40
+}
 int main()
 {
 	test1();//18
+	test11();//20
 	system("pause");
 	return 0;
 }
