3177bf9e1a76e60a044dc07d5d91990e8055124b
Fowindy <47002339@qq.com>
2020-10-22 00:39:18 +0800

    34_全局函数做友元

---
+++ Src/12_友元/34_全局函数做友元.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//建筑物类
+class Building
+{
+	//全局函数友元的声明
+	friend void goodGay(Building* building);
+public:
+	Building()
+	{
+		m_SittingRoom = "客厅";
+		m_BedRoom = "卧室";
+	}
+public:
+	string m_SittingRoom;//客厅
+private:
+	string m_BedRoom;//卧室
+};
+//全局函数
+void goodGay(Building* building)
+{
+	cout << "好基友全局函数正在访问:" << building->m_SittingRoom << endl;
+	cout << "好基友全局函数正在访问:" << building->m_BedRoom << endl;
+}
+void test4()
+{
+	Building building;
+	goodGay(&building);
+}
+int main34()
+{
+	test4();
+	system("pause");
+	return 0;
+}
