51ec6a5f5ae38c85db209fd8800cfc4409eb819f
Fowindy <47002339@qq.com>
2020-10-22 20:30:16 +0800

    38_5.利用构造函数传参

--- Src/13_运算符重载/38_左移运算符重载.cpp
+++ Src/13_运算符重载/38_左移运算符重载.cpp
@@ -4,6 +4,13 @@ using namespace std;
 //左移运算符重载
 class Person8
 {
+public:
+	//5.利用构造函数传参
+	Person8(int a,int b)
+	{
+		m_A = a;
+		m_B = b;
+	}
 	//4.利用友元访问私有成员变量
 	friend ostream& operator<<(ostream& out, Person8& p);
 	//利用成员函数重载 左移运算符 p.operator<<(cout) 简化版本 p<<cout
@@ -18,15 +25,15 @@ private:
 //3.只能利用全局函数重载左移运算符
 ostream& operator<<(ostream& out,Person8& p)
 {
-	p.m_A = 10;
-	p.m_B = 10;
+	//p.m_A = 10;
+	//p.m_B = 10;
 	//此处cout为别名可以更改为out其他所有的别名都可以
 	out<<"m_A = "<< p.m_A << ",m_B = "<<p.m_B<<endl;
 	return out;
 }
 void test8()
 {
-	Person8 p;
+	Person8 p(10,10);
 	cout<<p<<"hello world."<<endl;
 	//m_A = 10,m_B = 10
 	//hello world.
