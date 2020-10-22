741759d717d3f4ec148c2bad8b80a5765a70bb40
Fowindy <47002339@qq.com>
2020-10-22 11:37:57 +0800

    37_2.全局函数重载+号

--- Src/13_运算符重载/37_加号运算符重载.cpp
+++ Src/13_运算符重载/37_加号运算符重载.cpp
@@ -9,14 +9,22 @@ public:
 	int m_B;
 	//1.成员函数重载+号
 public:
-	Person7 operator+(Person7& p)
-	{
-		Person7 temp;
-		temp.m_A = this->m_A + p.m_A;
-		temp.m_B = this->m_B + p.m_B;
-		return temp;
-	}
+	//Person7 operator+(Person7& p)
+	//{
+	//	Person7 temp;
+	//	temp.m_A = this->m_A + p.m_A;
+	//	temp.m_B = this->m_B + p.m_B;
+	//	return temp;
+	//}
 };
+//2.全局函数重载+号
+Person7 operator+(Person7& p1, Person7& p2)
+{
+	Person7 temp;
+	temp.m_A = p1.m_A + p2.m_A;
+	temp.m_B = p1.m_B + p2.m_B;
+	return temp;
+}
 void test7()
 {
 	Person7 p1;
@@ -31,7 +39,6 @@ void test7()
 	cout << "p3.m_A = " << p3.m_A << endl;//25
 	cout << "p3.m_B = " << p3.m_B << endl;//25
 }
-//2.全局函数重载+号
 int main()
 {
 	test7();
