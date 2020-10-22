51565c12f9644a3ab32e528798c474c684942463
Fowindy <47002339@qq.com>
2020-10-22 20:27:40 +0800

    38_4.利用友元访问私有成员变量

--- Src/13_运算符重载/38_左移运算符重载.cpp
+++ Src/13_运算符重载/38_左移运算符重载.cpp
@@ -4,18 +4,22 @@ using namespace std;
 //左移运算符重载
 class Person8
 {
+	//4.利用友元访问私有成员变量
+	friend ostream& operator<<(ostream& out, Person8& p);
 	//利用成员函数重载 左移运算符 p.operator<<(cout) 简化版本 p<<cout
 	//2.不会利用成员函数重载<<运算符,因为无法实现cout在左侧
 	//void operator<<(Person8& p)
 	//{
 	//}
-public:
+private:
 	int m_A;
 	int m_B;
 };
 //3.只能利用全局函数重载左移运算符
 ostream& operator<<(ostream& out,Person8& p)
 {
+	p.m_A = 10;
+	p.m_B = 10;
 	//此处cout为别名可以更改为out其他所有的别名都可以
 	out<<"m_A = "<< p.m_A << ",m_B = "<<p.m_B<<endl;
 	return out;
@@ -23,8 +27,6 @@ ostream& operator<<(ostream& out,Person8& p)
 void test8()
 {
 	Person8 p;
-	p.m_A = 10;
-	p.m_B = 10;
 	cout<<p<<"hello world."<<endl;
 	//m_A = 10,m_B = 10
 	//hello world.
