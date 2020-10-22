0543bb8ac42ae879cf0a1fa2191ce05318565098
Fowindy <47002339@qq.com>
2020-10-22 20:25:43 +0800

    38_3.只能利用全局函数重载左移运算符

--- Src/13_运算符重载/38_左移运算符重载.cpp
+++ Src/13_运算符重载/38_左移运算符重载.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Person8
 {
 	//利用成员函数重载 左移运算符 p.operator<<(cout) 简化版本 p<<cout
-	//不会利用成员函数重载<<运算符,因为无法实现cout在左侧
+	//2.不会利用成员函数重载<<运算符,因为无法实现cout在左侧
 	//void operator<<(Person8& p)
 	//{
 	//}
@@ -13,12 +13,21 @@ public:
 	int m_A;
 	int m_B;
 };
+//3.只能利用全局函数重载左移运算符
+ostream& operator<<(ostream& out,Person8& p)
+{
+	//此处cout为别名可以更改为out其他所有的别名都可以
+	out<<"m_A = "<< p.m_A << ",m_B = "<<p.m_B<<endl;
+	return out;
+}
 void test8()
 {
 	Person8 p;
 	p.m_A = 10;
 	p.m_B = 10;
-	cout<<p.m_A<<endl;//10
+	cout<<p<<"hello world."<<endl;
+	//m_A = 10,m_B = 10
+	//hello world.
 }
 int main()
 {
