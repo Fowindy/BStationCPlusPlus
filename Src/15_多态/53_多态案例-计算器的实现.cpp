c5cc57841a1e82ae83389cd7b266aa2be8159661
Fowindy <47002339@qq.com>
2020-10-23 22:36:46 +0800

    53_1.利用多态实现计算器

--- Src/15_多态/53_多态案例-计算器的实现.cpp
+++ Src/15_多态/53_多态案例-计算器的实现.cpp
@@ -26,6 +26,85 @@ public:
 	int m_Num1;//操作数1
 	int m_Num2;//操作数2
 };
+//1.利用多态实现计算器
+//多态好处:
+//1、组织结构清晰
+//2、可读性强
+//3、对于前期和后期扩展以及维护性高
+//实现计算器抽象类
+class AbstractCalculator
+{
+public:
+	virtual int getResult()
+	{
+		return 0;
+	}
+	int m_Num1;//操作数1
+	int m_Num2;//操作数2
+};
+//加法计算器类
+class AddCalculator :public AbstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 + m_Num2;
+	}
+};
+//减法计算器类
+class SubCalculator :public AbstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 - m_Num2;
+	}
+};
+//乘法计算器类
+class ChengFaCalculator :public AbstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 * m_Num2;
+	}
+};
+//除法计算器类
+class ChuFaCalculator :public AbstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 / m_Num2;
+	}
+};
+void test531()
+{
+	AbstractCalculator* abc = new AddCalculator;
+	abc->m_Num1 = 10;
+	abc->m_Num2 = 20;
+	cout << abc->m_Num1 << " + " << abc->m_Num2 << " = " << abc->getResult() << endl;
+	//因为创建在堆区,用完记得销毁
+	delete abc;
+	//减法运算
+	abc = new SubCalculator;
+	abc->m_Num1 = 10;
+	abc->m_Num2 = 20;
+	cout << abc->m_Num1 << " - " << abc->m_Num2 << " = " << abc->getResult() << endl;
+	delete abc;
+	//乘法运算
+	abc = new ChengFaCalculator;
+	abc->m_Num1 = 10;
+	abc->m_Num2 = 20;
+	cout << abc->m_Num1 << " * " << abc->m_Num2 << " = " << abc->getResult() << endl;
+	delete abc;
+	//除法运算
+	abc = new ChuFaCalculator;
+	abc->m_Num1 = 10;
+	abc->m_Num2 = 20;
+	cout << abc->m_Num1 << " / " << abc->m_Num2 << " = " << abc->getResult() << endl;
+	delete abc;
+}
 void test53()
 {
 	Calculator calc;
@@ -42,7 +121,8 @@ void test53()
 }
 int main()
 {
-	test53();
+	//test53();
+	test531();
 	system("pause");
 	return 0;
 }
