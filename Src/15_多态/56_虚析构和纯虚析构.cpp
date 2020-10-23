3ee94016373880f4a2c0c80dbc402f3b96cc0573
Fowindy <47002339@qq.com>
2020-10-24 06:03:59 +0800

    56_2.?输出并没有出现Cat析构函数的调用，说明堆区数据没有释放干净，导致内存泄漏

--- Src/15_多态/56_虚析构和纯虚析构.cpp
+++ Src/15_多态/56_虚析构和纯虚析构.cpp
@@ -4,14 +4,24 @@ using namespace std;
 //虚析构和纯虚析构
 class Animal56
 {
+
 public:
+	Animal56()
+	{
+		cout << "Animal构造函数调用" << endl;
+	}
 	virtual void Speak() = 0;
+	~Animal56()
+	{
+		cout << "Animal析构函数调用" << endl;
+	}
 };
 class Cat56 :public Animal56
 {
 public:
 	Cat56(string name)
 	{
+		cout << "Cat构造函数调用" << endl;
 		m_Name = new string(name);
 	}
 	virtual void Speak()
@@ -19,17 +29,34 @@ public:
 		cout << *m_Name << "小猫会说话" << endl;
 	}
 	string* m_Name;
+	~Cat56()
+	{
+		if (m_Name != NULL)
+		{
+			//2.?输出并没有出现Cat析构函数的调用，说明堆区数据没有释放干净，导致内存泄漏
+			cout << "Cat析构函数调用" << endl;
+			delete m_Name;
+			m_Name = NULL;
+		}
+	}
 };
 void test56()
 {
 	Animal56* animal = new Cat56("Tom");
 	animal->Speak();
-	//1.初步释放堆区数据,最好是通过析构函数释放
+	//1.因为父类指针指向Cat并存放在堆区，需要手动释放
 	delete animal;
 }
 int main()
 {
 	test56();
 	system("pause");
+	/*
+	Animal构造函数调用
+	Cat构造函数调用
+	Tom小猫会说话
+	Animal析构函数调用
+	请按任意键继续. . .
+	*/
 	return 0;
 }
