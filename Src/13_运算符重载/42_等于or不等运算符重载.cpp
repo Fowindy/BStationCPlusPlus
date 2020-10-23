38f58b4585110ebbcf13d11ce9bb8e71a47f9f2c
Fowindy <47002339@qq.com>
2020-10-23 12:12:03 +0800

    42_2.相等运算符重载

--- Src/13_运算符重载/42_等于or不等运算符重载.cpp
+++ Src/13_运算符重载/42_等于or不等运算符重载.cpp
@@ -1,33 +1,47 @@
 ﻿#include <iostream>
 #include <string>
 using namespace std;
-class Person
+class Person42
 {
+private:
+	int m_Age;
+	string m_Name;
 public:
 	//构造函数初始化局部变量
-	Person(string name, int age)
+	Person42(string name, int age)
 	{
 		m_Age = age;
 		m_Name = name;
 	}
-private:
-	int m_Age;
-	string m_Name;
+	//2.相等运算符重载
+	bool operator==(Person42& p)
+	{
+		if (this->m_Age == p.m_Age && this->m_Name == p.m_Name)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
 };
-void test01()
+void test42()
 {
-	Person p1("Tom", 18);
-	Person p2("Tom", 18);
+	Person42 p1("Tom", 18);
+	Person42 p2("Tom", 19);
 	if (p1 == p2)
 	{
+		cout << "p1和p2相等" << endl;
 	}
 	else
 	{
-
+		cout << "p1和p2不相等" << endl;
 	}
 }
 int main()
 {
+	test42();
 	system("pause");
 	return 0;
 }
