777b320b03ab0b6c40f6619ad697485d880ac86d
Fowindy <47002339@qq.com>
2020-10-23 12:33:07 +0800

    43_2.函数运算符重载_int

--- Src/13_运算符重载/43_函数调用运算符重载.cpp
+++ Src/13_运算符重载/43_函数调用运算符重载.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 using namespace std;
-//函数调用运算符重载
+//函数调用运算符重载_伪函数
 class MyPrint
 {
 public:
@@ -10,10 +10,23 @@ public:
 		cout << text << endl;
 	}
 };
+class MyAdd
+{
+public:
+	//2.函数运算符重载_int
+	//伪函数非常灵活,没有固定写法
+	//加法
+	int operator()(int num1, int num2)
+	{
+		return num1 + num2;
+	}
+};
 void text43()
 {
 	MyPrint myPrint;
 	myPrint("hello world");//1.函数运算符的重载_string
+	MyAdd myAdd;
+	cout << myAdd(10, 26) << endl;
 }
 void myPrint(string text)
 {
