b248d7d379914cd61a7fac6f06c9b783b616741a
Fowindy <47002339@qq.com>
2020-10-23 22:51:40 +0800

    54_1.子类未重写父类中的纯虚函数,也属于抽象类

---
+++ Src/15_多态/54_纯虚函数和抽象类.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class Base54
+{
+	//纯虚函数
+	//语法：virtual 返回值类型 函数名 (参数) = 0;
+	//只要有一个纯虚函数,这个类称为抽象类
+	// 抽象类特点:
+	//1、无法实例化对象
+	//2、抽象类的子类 必须要重写父类中的纯虚函数,否则也属于抽象类
+public:
+	virtual void func() = 0;
+};
+class Son :public Base54
+{
+	//1.子类未重写父类中的纯虚函数,也属于抽象类
+public:
+};
+void test54()
+{
+	//Base b; //抽象类是无法实例化对象
+	//new Base;// 抽象类是无法实例化对象
+	//Son s; //抽象类是无法实例化对象
+	//new Son;// 抽象类是无法实例化对象
+}
+int main()
+{
+	system("pause");
+	return 0;
+}
