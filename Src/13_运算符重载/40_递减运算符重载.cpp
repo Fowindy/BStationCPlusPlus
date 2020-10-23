b12c453210c1a20144b97cc83b3bcc48bc116346
Fowindy <47002339@qq.com>
2020-10-23 10:18:22 +0800

    40_3.递减运算符(后置--)重载

--- Src/13_运算符重载/40_递减运算符重载.cpp
+++ Src/13_运算符重载/40_递减运算符重载.cpp
@@ -4,38 +4,50 @@ using namespace std;
 //递减运算符重载
 class MyInteger1
 {
-	//声明友元访问私有局部变量
-	friend ostream& operator<<(ostream& cout, MyInteger1& myNum);
+	friend ostream& operator<<(ostream& cout, MyInteger1 myInt);
 public:
-	//2.成员函数实现递减运算符(前置--)重载
+	//2.递减运算符(前置--)重载
 	MyInteger1& operator--()
 	{
-		//先自减
+		//先减
 		m_Num--;
-		//后返回自身对象
+		//后返回
 		return *this;
 	}
+	//3.递减运算符(后置--)重载
+	MyInteger1 operator--(int)
+	{
+		//先建临时变量存储对象
+		MyInteger1 temp = *this;
+		//后让值--
+		m_Num--;
+		//最后返回记录的对象
+		return temp;
+	}
 public:
-	//构造函数初始化值
 	MyInteger1()
 	{
-		m_Num = 0;
+		m_Num = 0;//构造函数初始化
 	}
 private:
 	int m_Num;
 };
-//1.左移运算符重载完成
-ostream& operator<<(ostream& cout, MyInteger1& myNum)
+//1.左移操作符重载
+ostream& operator<<(ostream& cout, MyInteger1 myInt)
 {
-	cout<<myNum.m_Num;
+	cout << myInt.m_Num;
 	return cout;
 }
 void test0()
 {
-	MyInteger1 myNum;
-	cout<<myNum<<endl;
-	cout<<"------------------(前置递减--)----------------"<<endl;
-	cout<<--myNum<<endl;
+	MyInteger1 myInt;
+	cout << myInt << endl;//0
+	cout << "------------------递减运算符(前置--)----------------" << endl;
+	cout << --myInt << endl;//-1
+	cout << myInt << endl;//-1
+	cout << "------------------递减运算符(后置--)----------------" << endl;
+	cout << myInt-- << endl;//-1
+	cout << myInt << endl;//-2
 }
 int main()
 {
