4de28fd57fa088284696562aad889de99c11773a
Fowindy <47002339@qq.com>
2020-10-23 18:15:44 +0800

    49_2.3.子类调用父类重载

--- Src/14_继承/49_继承同名静态成员处理方式.cpp
+++ Src/14_继承/49_继承同名静态成员处理方式.cpp
@@ -10,6 +10,11 @@ public:
 	{
 		cout << "Base 下 func()的调用" << endl;
 	}
+	//父类同名重载的调用
+	static void func(int a)
+	{
+		cout << "Base 下 func(int a)的调用" << endl;
+	}
 };
 class Son491 :public Base49
 {
@@ -49,6 +54,10 @@ void test491()
 	cout << "------------------通过类名访问----------------" << endl;
 	Son491::func();
 	Son491::Base49::func();
+	//2.3.子类调用父类重载
+	// 子类出现和父类同名静态成员函数, 也会隐藏父类中所有同名成员函数
+	//	如果想访问父类中被隐藏同名成员, 需要加作用域
+	Son491::Base49::func(49);
 }
 int main()
 {
