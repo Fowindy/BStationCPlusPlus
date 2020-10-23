212d605108bb087234a58c47d92804da61b9b0aa
Fowindy <47002339@qq.com>
2020-10-23 11:31:00 +0800

    41_1.p2赋值操作p1

--- Src/13_运算符重载/41_递减运算符重载.cpp
+++ Src/13_运算符重载/41_递减运算符重载.cpp
@@ -20,7 +20,10 @@ private:
 void test01()
 {
 	Person p1(18);
-	cout << "p1的年龄为:" << *p1.m_Age << endl;
+	Person p2(20);
+	p2 = p1;//1.p2赋值操作p1
+	cout << "p1的年龄为:" << *p1.m_Age << endl;//18
+	cout << "p2的年龄为:" << *p2.m_Age << endl;//20//18
 }
 int main()
 {
