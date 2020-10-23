1eff83f62399fd66bd347c673b28cbab6d44ddbb
Fowindy <47002339@qq.com>
2020-10-23 19:00:24 +0800

    51_2.利用虚继承,解决菱形继承的问题

--- Src/14_继承/51_菱形继承.cpp
+++ Src/14_继承/51_菱形继承.cpp
@@ -7,10 +7,13 @@ class Animal
 public:
 	int m_Age;
 };
-class Sheep :public Animal
+//2.利用虚继承,解决菱形继承的问题
+//语法:在继承之前 加上关键字 virtual 变为虚继承
+//Animal类称为 虚基类
+class Sheep :virtual public Animal
 {
 };
-class Tuo :public Animal
+class Tuo :virtual public Animal
 {
 };
 class SheepTuo :public Sheep, public Tuo
@@ -22,8 +25,11 @@ void test51()
 	st.Sheep::m_Age = 18;
 	st.Tuo::m_Age = 28;
 	//1.当菱形继承,两个父类拥有相同数据,需要加以作用域区分
-	cout << "st.Sheep::m_Age = " << st.Sheep::m_Age << endl;//18
+	cout << "st.Sheep::m_Age = " << st.Sheep::m_Age << endl;//28
 	cout << "st.Tuo::m_Age = " << st.Tuo::m_Age << endl;//28
+
+	//这份数据我们知道只有有一份就可以,菱形维承导致数据有两份,资源浪费
+	cout << "st.m_Age = " << st.m_Age << endl;//28
 }
 int main()
 {
