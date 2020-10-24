1a61f5478fb63159663e09dfd69cd8fd95a0685e
Fowindy <47002339@qq.com>
2020-10-24 12:43:52 +0800

    屏蔽测试代码

--- Src/17_基于多态的职工管理系统/职工管理系统.cpp
+++ Src/17_基于多态的职工管理系统/职工管理系统.cpp
@@ -9,57 +9,57 @@ using namespace std;
 int main()
 {
 #pragma region 测试代码
-	cout << "------------------员工类测试----------------" << endl;
-	Worker* worker = new Employee(1001, "张三", "男", 18, 1000);
-	worker->ShowWorkerInfo();
-	delete worker;
-	cout << "------------------经理类测试----------------" << endl;
-	Worker* worker1 = new Manager(0011, "李四", "男", 28, 0000);
-	worker1->ShowWorkerInfo();
-	delete worker1;
-	cout << "------------------经理类测试----------------" << endl;
-	Worker* worker0 = new Boss(0001, "马云", "男", 38, 0000);
-	worker0->ShowWorkerInfo();
-	delete worker0;
+	//cout << "------------------员工类测试----------------" << endl;
+	//Worker* worker = new Employee(1001, "张三", "男", 18, 1000);
+	//worker->ShowWorkerInfo();
+	//delete worker;
+	//cout << "------------------经理类测试----------------" << endl;
+	//Worker* worker1 = new Manager(0011, "李四", "男", 28, 0000);
+	//worker1->ShowWorkerInfo();
+	//delete worker1;
+	//cout << "------------------经理类测试----------------" << endl;
+	//Worker* worker0 = new Boss(0001, "马云", "男", 38, 0000);
+	//worker0->ShowWorkerInfo();
+	//delete worker0;
 #pragma endregion
 #pragma region 正式代码
-	////实例化管理类的对象
-	//WorkerManager workerManager;
-	//while (true)
-	//{
-	//	//调用展示菜单的成员函数
-	//	workerManager.ShowMenu();
-	//	//提示用户输入选项
-	//	cout << "请输入您的选择(0-9):" << endl;
-	//	//用来存储用户选项
-	//	int _choice = 0;
-	//	//接收用户的选项
-	//	cin >> _choice;
-	//	//switch执行选项分支
-	//	switch (_choice)
-	//	{
-	//	case 0:	//退出系统
-	//		workerManager.ExitSystem();
-	//		break;
-	//	case 1:	//增加职工
-	//		break;
-	//	case 2:	//显示职工
-	//		break;
-	//	case 3:	//删除职工
-	//		break;
-	//	case 4:	//修改职工
-	//		break;
-	//	case 5:	//查找职工
-	//		break;
-	//	case 6:	//排序职工
-	//		break;
-	//	case 7:	//清空文档
-	//		break;
-	//	default:
-	//		system("cls");	//任何其他选择都清空屏幕再显示(刷新屏幕)
-	//		break;
-	//	}
-	//}
+	//实例化管理类的对象
+	WorkerManager workerManager;
+	while (true)
+	{
+		//调用展示菜单的成员函数
+		workerManager.ShowMenu();
+		//提示用户输入选项
+		cout << "请输入您的选择(0-9):" << endl;
+		//用来存储用户选项
+		int _choice = 0;
+		//接收用户的选项
+		cin >> _choice;
+		//switch执行选项分支
+		switch (_choice)
+		{
+		case 0:	//退出系统
+			workerManager.ExitSystem();
+			break;
+		case 1:	//增加职工
+			break;
+		case 2:	//显示职工
+			break;
+		case 3:	//删除职工
+			break;
+		case 4:	//修改职工
+			break;
+		case 5:	//查找职工
+			break;
+		case 6:	//排序职工
+			break;
+		case 7:	//清空文档
+			break;
+		default:
+			system("cls");	//任何其他选择都清空屏幕再显示(刷新屏幕)
+			break;
+		}
+	}
 #pragma endregion
 
 	system("pause");
