568ef96ae546772ff6643c919cd966e75cb9c16b
Fowindy <47002339@qq.com>
2020-10-24 11:55:14 +0800

    员工类完成

---
+++ Src/17_基于多态的职工管理系统/Employee.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include "Employee.h"
+using namespace std;
+Employee::Employee(int id, string name, string sex, int age, int deptId)
+{
+	this->m_Id = id;
+	this->m_Name = name;
+	this->m_Sex = sex;
+	this->m_Age = age;
+	this->m_DeptId = deptId;
+}
+void Employee::ShowWorkerInfo()
+{
+	cout << "职工编号:" << this->m_Id
+		<< "\t职工姓名:" << this->m_Name
+		<< "\t职工性别:" << this->m_Sex
+		<< "\t职工年龄:" << this->m_Age
+		<< "\t职工部门编号:" << this->m_DeptId
+		<< "\t职工职位:" << this->getDeptName()
+		<< "\t岗位职责:完成经理交给的任务"
+		<< endl;
+}
+string Employee::getDeptName()
+{
+	return string("员工");
+}
+Employee::~Employee()
+{
+
+}
--- Src/17_基于多态的职工管理系统/职工管理系统.cpp
+++ Src/17_基于多态的职工管理系统/职工管理系统.cpp
@@ -2,45 +2,55 @@
 #include <string>
 using namespace std;
 #include "WorkerManager.h"
+#include "Worker.h"
+#include "Employee.h"
 int main()
 {
-	//实例化管理类的对象
-	WorkerManager workerManager;
-	while (true)
-	{
-		//调用展示菜单的成员函数
-		workerManager.ShowMenu();
-		//提示用户输入选项
-		cout << "请输入您的选择(0-9):" << endl;
-		//用来存储用户选项
-		int _choice = 0;
-		//接收用户的选项
-		cin >> _choice;
-		//switch执行选项分支
-		switch (_choice)
-		{
-		case 0:	//退出系统
-			workerManager.ExitSystem();
-			break;
-		case 1:	//增加职工
-			break;
-		case 2:	//显示职工
-			break;
-		case 3:	//删除职工
-			break;
-		case 4:	//修改职工
-			break;
-		case 5:	//查找职工
-			break;
-		case 6:	//排序职工
-			break;
-		case 7:	//清空文档
-			break;
-		default:
-			system("cls");	//任何其他选择都清空屏幕再显示(刷新屏幕)
-			break;
-		}
-	}
+#pragma region 测试代码
+	Worker* worker = new Employee(1001, "张三", "男", 18, 1000);
+	worker->ShowWorkerInfo();
+	delete worker;
+#pragma endregion
+#pragma region 正式代码
+	////实例化管理类的对象
+	//WorkerManager workerManager;
+	//while (true)
+	//{
+	//	//调用展示菜单的成员函数
+	//	workerManager.ShowMenu();
+	//	//提示用户输入选项
+	//	cout << "请输入您的选择(0-9):" << endl;
+	//	//用来存储用户选项
+	//	int _choice = 0;
+	//	//接收用户的选项
+	//	cin >> _choice;
+	//	//switch执行选项分支
+	//	switch (_choice)
+	//	{
+	//	case 0:	//退出系统
+	//		workerManager.ExitSystem();
+	//		break;
+	//	case 1:	//增加职工
+	//		break;
+	//	case 2:	//显示职工
+	//		break;
+	//	case 3:	//删除职工
+	//		break;
+	//	case 4:	//修改职工
+	//		break;
+	//	case 5:	//查找职工
+	//		break;
+	//	case 6:	//排序职工
+	//		break;
+	//	case 7:	//清空文档
+	//		break;
+	//	default:
+	//		system("cls");	//任何其他选择都清空屏幕再显示(刷新屏幕)
+	//		break;
+	//	}
+	//}
+#pragma endregion
+
 	system("pause");
 	return 0;
 }
