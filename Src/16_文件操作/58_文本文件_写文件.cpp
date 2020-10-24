3bb6a37b679e0ec124c4283535b4cef7d8097851
Fowindy <47002339@qq.com>
2020-10-24 08:09:57 +0800

    58_5.关闭写入流

--- Src/16_文件操作/58_文本文件_写文件.cpp
+++ Src/16_文件操作/58_文本文件_写文件.cpp
@@ -9,14 +9,17 @@ void test58()
 	//2.创建写入流对象
 	ofstream ofs;
 	//3.指定打开目录,打开方式
-	ofs.open("test.txt", ios::out);
+	ofs.open("test.txt", ios::out);//文件输出位置在当前cpp所在目录
 	//4.写内容
 	ofs << "姓名:张三" << endl;
 	ofs << "性别:男" << endl;
 	ofs << "年龄:19" << endl;
+	//5.关闭写入流
+	ofs.close();
 }
 int main()
 {
+	test58();
 	system("pause");
 	return 0;
 }
