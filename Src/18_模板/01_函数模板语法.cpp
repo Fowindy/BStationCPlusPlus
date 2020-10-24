9c2f950fc765bf269a3485f688c7ff8bca61e4a6
Fowindy <47002339@qq.com>
2020-10-25 06:19:39 +0800

    模板语法

---
+++ Src/18_模板/01_函数模板语法.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//交换整型函数
+void swapInt(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+//交换双精度函数
+void swapDouble(double& a, double& b)
+{
+	double temp = a;
+	a = b;
+	b = temp;
+}
+//模板语法:
+//template<typename T>	//函数声明或定义
+//利用模板提供通用的交换函数
+template<typename T>	//模板函数声明
+void mySwap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+void test01()
+{
+	int a = 10;
+	int b = 20;
+	swapInt(a, b);
+}
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
