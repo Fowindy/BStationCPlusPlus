5c29988716b05bedd7094462049550aba922cd37
Fowindy <47002339@qq.com>
2020-10-24 08:07:41 +0800

    58_4.写内容

--- Src/16_文件操作/58_文本文件_写文件.cpp
+++ Src/16_文件操作/58_文本文件_写文件.cpp
@@ -10,6 +10,10 @@ void test58()
 	ofstream ofs;
 	//3.指定打开目录,打开方式
 	ofs.open("test.txt", ios::out);
+	//4.写内容
+	ofs << "姓名:张三" << endl;
+	ofs << "性别:男" << endl;
+	ofs << "年龄:19" << endl;
 }
 int main()
 {
