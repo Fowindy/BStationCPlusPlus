7e6c3a16d9a15ad68548de7c45ad26ecdd9d75ee
Fowindy <47002339@qq.com>
2020-10-27 10:11:23 +0800

    类模板在模板参数列表中可以有默认参数_1

--- Src/18_模板/08_类模板和函数模板的区别.cpp
+++ Src/18_模板/08_类模板和函数模板的区别.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 //类模板和函数模板的区别
-template<class NameType, class AgeType>
+template<class NameType, class AgeType = int>
 class Person08
 {
 public:
@@ -23,9 +23,13 @@ void test08()
 	//1.类模板没有自动类型推导的功能,只能用显式指定类型
 	//Person08 p("孙悟空", 999);//错误:无法用自动类型推导
 	Person08<string, int> p("孙悟空", 999);//正确:只能用显式指定类型
+	//2.类模板在模板参数列表中可以有默认参数_1
+	Person08<string>p1("猪八戒", 356);//name:猪八戒;age:356
+	p1.showPerson();
 }
 int main()
 {
+	test08();
 	system("pause");
 	return 0;
 }
