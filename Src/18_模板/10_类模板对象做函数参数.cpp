7ad15d590598d89ccf9b10e1e135bfd90e15a294
Fowindy <47002339@qq.com>
2020-10-27 19:42:43 +0800

    调类模板的对象做函数的参数

---
+++ Src/18_模板/10_类模板对象做函数参数.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//类模板对象做函数参数
+//1. 指定传入的类型-- - 直接显示对象的数据类型
+//2. 参数模板化-- - 将对象中的参数变为模板进行传递
+//3. 整个类模板化-- - 将这个对象类型 模板化进行传递
+template<typename NameType, typename AgeType>
+class Person10
+{
+public:
+	Person10(NameType name, AgeType age)
+	{
+		this->m_Name = name;
+		this->m_Age = age;
+	}
+	void ShowPerson()
+	{
+		cout << "姓名:" << this->m_Name << ";年龄:" << this->m_Age << endl;
+	}
+	NameType m_Name;
+	AgeType m_Age;
+};
+//************************************
+// Method:    PrintPerson
+// Access:    public 
+// Returns:   void
+// Author: 	  Fowindy
+// Parameter: Person10<string
+// Parameter: int> & p
+// Created:   2020/10/27 19:40
+//************************************
+//调类模板的对象做函数的参数
+void PrintPerson(Person10<string, int>& p)
+{
+	p.ShowPerson();
+}
+void test10()
+{
+	Person10<string, int>p("孙悟空", 999);
+	PrintPerson(p);//姓名:孙悟空;年龄:999
+}
+int main()
+{
+	test10();
+	system("pause");
+	return 0;
+}
