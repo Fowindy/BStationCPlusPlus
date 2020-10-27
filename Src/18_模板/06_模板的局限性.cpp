55102201d4facad7741116ac370d57df28f26b7e
Fowindy <47002339@qq.com>
2020-10-27 09:38:03 +0800

    利用具体化Person的版本实现代码,具体化优先调用(重载)

--- Src/18_模板/06_模板的局限性.cpp
+++ Src/18_模板/06_模板的局限性.cpp
@@ -4,6 +4,20 @@ using namespace std;
 //模板的局限性
 //模板的通用性并不是万能的,有些特定数据类型,需要用具体化方式做特殊实现
 
+class Person06
+{
+public:
+	Person06(string name, int age)
+	{
+		this->m_Name = name;
+		this->m_Age = age;
+	}
+	//姓名
+	string m_Name;
+	//年龄
+	int m_Age;
+};
+
 //对比两个数据是否相等
 //************************************
 // Method:    IsEqual
@@ -15,10 +29,32 @@ using namespace std;
 // Created:   2020/10/27 1:03
 //************************************
 template<typename T>
-bool IsEqual(T a, T b)
+bool IsEqual(T &a, T &b)
 {
 	return a == b ? true : false;
 }
+//利用具体化Person的版本实现代码,具体化优先调用(重载)
+//************************************
+// Method:    IsEqual
+// Access:    public 
+// Returns:   bool
+// Author: 	  Fowindy
+// Parameter: Person06 & a
+// Parameter: Person06 & b
+// Created:   2020/10/27 9:40
+//************************************
+template<> bool IsEqual(Person06 &a, Person06 &b)
+{
+	if (a.m_Name == b.m_Name && a.m_Age == b.m_Age)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 void test06()
 {
 	int a = 10;
@@ -32,9 +68,23 @@ void test06()
 		cout << "a != b" << endl;
 	}
 }
+void test006()
+{
+	Person06 p1("Tom", 10);
+	Person06 p2("Tom", 10);
+	if (IsEqual(p1, p2))//因为p1和p2属于自定义Person类型,模板的局限性
+	{
+		cout << "对象p1 == 对象p2" << endl;
+	}
+	else
+	{
+		cout << "对象p1 != 对象p2" << endl;
+	}
+}
 int main()
 {
 	test06();
+	test006();
 	system("pause");
 	return 0;
 }
