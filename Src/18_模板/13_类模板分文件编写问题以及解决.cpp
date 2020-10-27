4f2aef1d1d78ba58af0ce38ff88969906e318ba5
Fowindy <47002339@qq.com>
2020-10-28 07:26:44 +0800

    解决方式2:将.h和.cpp中的内容写到一起,将后缀名改为.hpp文件(.hpp为类模板后缀)

--- Src/18_模板/13_类模板分文件编写问题以及解决.cpp
+++ Src/18_模板/13_类模板分文件编写问题以及解决.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
 #include <string>
 using namespace std;
-#include "Person13.cpp"//解决方式1：直接包含.cpp源文件
+//#include "Person13.cpp"//解决方式1：直接包含.cpp源文件
+#include "Person13.hpp"//解决方式2:将.h和.cpp中的内容写到一起,将后缀名改为.hpp文件(.hpp为类模板后缀)
 //13_类模板分文件编写问题以及解决
 //template<typename T1, typename T2>
 //class Person13
---
+++ Src/18_模板/Person13.hpp
@@ -0,0 +1,30 @@
+﻿#pragma once	//防止头文件重复包含
+#include <iostream>	//包含输入输出流的头文件
+using namespace std;	//使用标准命名空间
+
+template<typename T1, typename T2>
+class Person13
+{
+public:
+	Person13(T1 name, T2 age);
+	void ShowPeron();
+	T1 m_Name;
+	T2 m_Age;
+	~Person13();	//析构函数
+};
+template<typename T1, typename T2>
+Person13<T1, T2>::Person13(T1 name, T2 age)
+{
+	this->m_Name = name;
+	this->m_Age = age;
+}
+template<typename T1, typename T2>
+Person13<T1, T2>::~Person13()
+{
+
+}
+template<typename T1, typename T2>
+void Person13<T1, T2>::ShowPeron()
+{
+	cout << "姓名:" << this->m_Name << ";年龄:" << this->m_Age << endl;
+}
