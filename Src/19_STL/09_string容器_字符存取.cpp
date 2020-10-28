93ccc0c57066710e73b73d3ecf8507b5eef0617b
Fowindy <47002339@qq.com>
2020-10-28 19:13:18 +0800

    3.修改单个字符

--- Src/19_STL/09_string容器_字符存取.cpp
+++ Src/19_STL/09_string容器_字符存取.cpp
@@ -9,15 +9,18 @@ void test09()
 	//1.通过[]访问单个字符
 	for (int i = 0; i < str.size(); i++)
 	{
-		cout << str[i] << " ";
+		cout << str[i] << " ";//h e l l o
 	}
 	cout << endl;
 	//2.通过at方式访问单个字符
 	for (int i = 0; i < str.size(); i++)
 	{
-		cout << str.at(i) << " ";
+		cout << str.at(i) << " ";//h e l l o
 	}
 	cout << endl;
+	//3.修改单个字符
+	str[0] = 'x';
+	cout << "str = " << str << endl;//str = xello
 }
 int main()
 {
