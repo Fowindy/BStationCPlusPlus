54d483d55929b173c04c556964de1922f086039d
Fowindy <47002339@qq.com>
2020-10-28 13:34:27 +0800

    创建了一个vector容器,可以理解为数组

---
+++ Src/19_STL/01_STL初识-vector存放内置数据类型.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+//vector容器存放内置数据类型
+void test01()
+{
+	//创建了一个vector容器,可以理解为数组
+	vector<int> v;
+}
+int main()
+{
+	system("pause");
+	return 0;
+}
