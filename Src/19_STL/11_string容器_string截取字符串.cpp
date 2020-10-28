00efe44f0b39196808c3077dadbf308ba35d13c9
Fowindy <47002339@qq.com>
2020-10-28 19:28:11 +0800

    案例:截取邮箱用户名

--- Src/19_STL/11_string容器_string截取字符串.cpp
+++ Src/19_STL/11_string容器_string截取字符串.cpp
@@ -8,9 +8,18 @@ void test11()
 	string subStr = str.substr(1, 3);
 	cout << "subStr = " << subStr << endl;//subStr = bcd
 }
+//案例:截取邮箱用户名
+void test011()
+{
+	string email = "lisi@sina.com";
+	int pos = email.find('@');
+	string userName = email.substr(0, pos);
+	cout << "userName = " << userName << endl;//userName = lisi
+}
 int main()
 {
 	test11();
+	test011();
 	system("pause");
 	return 0;
 }
