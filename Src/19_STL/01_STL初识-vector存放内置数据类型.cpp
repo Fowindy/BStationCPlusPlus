46a4fdac1d7f558ee517740cd3d4886258ae5518
Fowindy <47002339@qq.com>
2020-10-28 13:34:37 +0800

    向容器中插入数据

--- Src/19_STL/01_STL初识-vector存放内置数据类型.cpp
+++ Src/19_STL/01_STL初识-vector存放内置数据类型.cpp
@@ -7,6 +7,12 @@ void test01()
 {
 	//创建了一个vector容器,可以理解为数组
 	vector<int> v;
+	//向容器中插入数据
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+	v.push_back(50);
 }
 int main()
 {
