dcf305ec54c3983801173cbabc827548a783829f
Fowindy <47002339@qq.com>
2020-10-28 09:00:19 +0800

    拷贝构造:防止delete掉申请的堆区内存,那么另一个指针将会变成野指针

--- Src/18_模板/MyArray.hpp
+++ Src/18_模板/MyArray.hpp
@@ -9,9 +9,26 @@ public:
 	MyArray(int capacity)
 	{
 		this->m_Capacity = capacity;
-		this->size = 0;//开始数组中的元素为0
+		this->m_size = 0;//开始数组中的元素为0
 		//按照用户申请的容量开辟数组空间
 		this->pArrayAddress = new T[this->m_Capacity];
+	}
+	//拷贝构造:防止delete掉申请的堆区内存,那么另一个指针将会变成野指针
+	MyArray(const MyArray& arr)
+	{
+		this->m_Capacity = arr.m_Capacity;
+		this->m_size = arr.m_size;
+		//浅拷贝带来的问题:堆区的内存重复释放
+		//this->pArrayAddress = arr.pArrayAddress;
+
+		//深拷贝
+		this->pArrayAddress = new T[arr.m_Capacity];
+		//深拷贝,如果之前arr数组已经有数据,还需要将数据拷贝过来
+		for (int i = 0; i < this->m_size; i++)
+		{
+			//数组元素的传递
+			this->pArrayAddress[i] = arr.pArrayAddress[i];
+		}
 	}
 	//因为在堆区开辟空间,所以需要提供析构函数手动释放资源
 	~MyArray()
@@ -26,7 +43,7 @@ public:
 private:
 	T* pArrayAddress;//指针指向堆区开辟的真实数组的首地址
 	int m_Capacity;//数组的容量
-	int size;//数组的真实大小  
+	int m_size;//数组的真实大小  
 #pragma endregion
 
 };
