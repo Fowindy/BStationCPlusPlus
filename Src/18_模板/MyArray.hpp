5c0d4bada7a6c69d5f8ad6c450d2d59102c22b5c
Fowindy <47002339@qq.com>
2020-10-28 08:42:06 +0800

    有参构造,参数:容量

--- Src/18_模板/MyArray.hpp
+++ Src/18_模板/MyArray.hpp
@@ -4,7 +4,15 @@ using namespace std;
 template<typename T>
 class MyArray
 {
-
+public:
+	//有参构造,参数:容量
+	MyArray(int capacity)
+	{
+		this->m_Capacity = capacity;
+		this->size = 0;//开始数组中的元素为0
+		//按照用户申请的容量开辟数组空间
+		this->pArrayAddress = new T[this->m_Capacity];
+	}
 #pragma region 私有属性定义
 private:
 	T* pArrayAddress;//指针指向堆区开辟的真实数组的首地址
