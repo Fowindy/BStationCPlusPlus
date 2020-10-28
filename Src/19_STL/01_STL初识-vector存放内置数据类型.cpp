d733a1397e0f581b13d41773c239e767be55d842
Fowindy <47002339@qq.com>
2020-10-28 13:49:25 +0800

    第三种遍历方式:利用STL提供的for_each遍历算法

--- Src/19_STL/01_STL初识-vector存放内置数据类型.cpp
+++ Src/19_STL/01_STL初识-vector存放内置数据类型.cpp
@@ -1,8 +1,13 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 //vector容器存放内置数据类型
+void myPrint(int val)
+{
+	cout << val << endl;
+}
 void test01()
 {
 	//创建了一个vector容器,可以理解为数组
@@ -22,11 +27,13 @@ void test01()
 	//	cout << *itBegin << endl;//接引用获取迭代器指针中的内容
 	//	itBegin++;//迭代器指针右移
 	//}
-	//第二种遍历方式:for循环
-	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
-	{
-		cout << *it << endl;
-	}
+	////第二种遍历方式:for循环
+	//for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	//{
+	//	cout << *it << endl;
+	//}
+	//第三种遍历方式:利用STL提供的for_each遍历算法 头文件 algorithm
+	for_each(v.begin(), v.end(), myPrint);
 }
 int main()
 {
