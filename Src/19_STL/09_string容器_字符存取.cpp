334f91ffd23596a07adfddc5cc1ef9f10c7a2922
Fowindy <47002339@qq.com>
2020-10-28 19:09:59 +0800

    1.通过[]访问单个字符

---
+++ Src/19_STL/09_string容器_字符存取.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//string字符存取
+void test09()
+{
+	string str = "hello";
+	cout << "str = " << str << endl;//hello
+	//1.通过[]访问单个字符
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << str[i] << " ";
+	}
+	cout << endl;
+}
+int main()
+{
+	test09();
+	system("pause");
+	return 0;
+}
