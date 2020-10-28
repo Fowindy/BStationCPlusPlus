dfd0bfa5e24c2118a46c2f3c8d64365af82c15ee
Fowindy <47002339@qq.com>
2020-10-28 13:39:43 +0800

    通过迭代器访问容器中的数据

--- Src/19_STL/01_STL初识-vector存放内置数据类型.cpp
+++ Src/19_STL/01_STL初识-vector存放内置数据类型.cpp
@@ -13,6 +13,9 @@ void test01()
 	v.push_back(30);
 	v.push_back(40);
 	v.push_back(50);
+	//通过迭代器访问容器中的数据
+	vector<int>::iterator itBegin = v.begin();//起始迭代器 指向容器中第一个元素的地址
+	vector<int>::iterator itEnd = v.end();//结束迭代器 指向容器中最后一个元素的下一个位置
 }
 int main()
 {
