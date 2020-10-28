d90f547e60f1fb38dc1a353919c562e380c7bceb
Fowindy <47002339@qq.com>
2020-10-28 11:43:33 +0800

    自定义类测试

--- Src/18_模板/15_类模板案例_数组类封装.cpp
+++ Src/18_模板/15_类模板案例_数组类封装.cpp
@@ -11,6 +11,28 @@ void PrintArray(T t)
 		cout << "第" << i + 1 << "个元素为:" << t[i] << endl;
 	}
 }
+//自定义类测试
+class Person15
+{
+public:
+	friend void printPersonArray(MyArray<Person15>& arr);
+	Person15() {}
+	Person15(string name, int age)
+	{
+		this->m_Name = name;
+		this->m_Age = age;
+	}
+private:
+	string m_Name;
+	int m_Age;
+};
+void printPersonArray(MyArray<Person15>& arr)
+{
+	for (int i = 0; i < arr.GetSize(); i++)
+	{
+		cout << "姓名:" << arr[i].m_Name << ";年龄:" << arr[i].m_Age << endl;
+	}
+}
 void test15()
 {
 	MyArray<int> arr1(5);//MyArray的有参构造
@@ -40,9 +62,22 @@ void test15()
 	//打印arr2中的最后插入的数据
 	cout << "arr2中最后插入的元素为:" << arr2[arr2.GetSize() - 1] << endl;
 }
+void test015()
+{
+	MyArray<Person15> arr(5);
+	arr.Push_Back(Person15("孙悟空", 999));
+	arr.Push_Back(Person15("赵云", 200));
+	arr.Push_Back(Person15("关羽", 88));
+	arr.Push_Back(Person15("妲己", 666));
+	arr.Push_Back(Person15("嫦娥", 777));
+	printPersonArray(arr);
+	cout << "arr的容量为:" << arr.GetCapacity() << endl;
+	cout << "arr的大小为:" << arr.GetSize() << endl;
+}
 int main()
 {
 	test15();
+	test015();
 	system("pause");
 	return 0;
 }
