0d9c22dc6903f11cef4a72cdf283a31381130cad
Fowindy <47002339@qq.com>
2020-10-29 19:07:27 +0800

    set容器的特点:所有元素插入时会自动排序;有重复值只会保留一个

--- Src/19_STL/35_set容器_构造和赋值.cpp
+++ Src/19_STL/35_set容器_构造和赋值.cpp
@@ -6,6 +6,14 @@
 #include <algorithm>
 #include <set>
 using namespace std;
+void PrintSet35(const set<int>& s)
+{
+	for (set<int>::const_iterator it = s.begin(); it != s.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
 void test35()
 {
 	set<int>s1;
@@ -15,9 +23,13 @@ void test35()
 	s1.insert(30);
 	s1.insert(20);
 	s1.insert(30);
+	//遍历容器
+	//set容器的特点:所有元素插入时会自动排序;有重复值只会保留一个
+	PrintSet35(s1);//10 20 30 40
 }
 int main()
 {
+	test35();
 	system("pause");
 	return 0;
 }
