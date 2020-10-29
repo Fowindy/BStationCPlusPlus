8e93d0b7b0ca309108a04ec06364f30291e6c9e6
Fowindy <47002339@qq.com>
2020-10-29 19:34:27 +0800

    统计元素:对set而言,统计结果要么是0要么是1

--- Src/19_STL/38_set容器_查找和统计.cpp
+++ Src/19_STL/38_set容器_查找和统计.cpp
@@ -13,7 +13,9 @@ void test38()
 	//插入
 	s1.insert(10);
 	s1.insert(30);
+	s1.insert(30);
 	s1.insert(20);
+	s1.insert(30);
 	s1.insert(40);
 	//查找元素
 	set<int>::iterator pos = s1.find(30);
@@ -25,6 +27,10 @@ void test38()
 	{
 		cout << "没有找到元素" << endl;
 	}
+
+	//统计元素:对set而言,统计结果要么是0要么是1
+	int num = s1.count(30);
+	cout << "num = " << num << endl;//1
 }
 int main()
 {
