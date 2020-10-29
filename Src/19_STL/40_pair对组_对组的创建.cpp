d48c97db457c16f5ff0ed340b6ad6a51b97115ce
Fowindy <47002339@qq.com>
2020-10-29 19:54:38 +0800

    pair对组创建的第二种方式

--- Src/19_STL/40_pair对组_对组的创建.cpp
+++ Src/19_STL/40_pair对组_对组的创建.cpp
@@ -11,6 +11,9 @@ void test40()
 	//pair对组创建的第一种方式
 	pair<string, int>p("Tom", 20);
 	cout << "姓名:" << p.first << "  年龄:" << p.second << endl;//姓名:Tom  年龄:20
+	//pair对组创建的第二种方式
+	pair<string, int> p2 = make_pair("Jerry", 30);
+	cout << "姓名:" << p2.first << "  年龄:" << p2.second << endl;//姓名:Jerry  年龄:30
 }
 int main()
 {
