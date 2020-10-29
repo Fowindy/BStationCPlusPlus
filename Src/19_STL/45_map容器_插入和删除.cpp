f6eefaaa52bde35174c432d982f959893daaa5a7
Fowindy <47002339@qq.com>
2020-10-29 21:28:05 +0800

    插入_第一种

---
+++ Src/19_STL/45_map容器_插入和删除.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+//45_map容器_插入和删除
+void test45()
+{
+	map<int, int>m;
+	//插入_第一种
+	m.insert(pair<int, int>(1, 10));
+}
+int main()
+{
+	system("pause");
+	return 0;
+}
