443079d72f743a3abce720a6f5ac17a821f35b84
Fowindy <47002339@qq.com>
2020-10-29 18:00:03 +0800

    访问第一个和最后元素

--- Src/19_STL/32_list容器_数据存取.cpp
+++ Src/19_STL/32_list容器_数据存取.cpp
@@ -16,9 +16,13 @@ void test32()
 	//L1[0] 不可用[]访问list容器中的元素
 	//L1.at(0) 不可用at方式访问list容器中的元素
 	//原因是list本质上是链表,不是用连续线性空间存储数据,迭代器是不支持随机访问的
+	//访问第一个和最后元素
+	cout << "第一个元素为:" << L1.front() << endl;//10
+	cout << "最后一个元素为:" << L1.back() << endl;//40
 }
 int main()
 {
+	test32();
 	system("pause");
 	return 0;
 }
