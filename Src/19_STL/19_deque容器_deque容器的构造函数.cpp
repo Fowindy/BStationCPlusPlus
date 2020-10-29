060287b55815d4e9bfda7ae7c6218c2da6d63cff
Fowindy <47002339@qq.com>
2020-10-29 09:47:21 +0800

    n个element方式赋值

--- Src/19_STL/19_deque容器_deque容器的构造函数.cpp
+++ Src/19_STL/19_deque容器_deque容器的构造函数.cpp
@@ -26,6 +26,9 @@ void test19()
 	//区间的方式赋值
 	deque<int> d2(d1.begin(), d1.end());
 	PrintDeque(d2);//0 1 2 3 4 5 6 7 8 9
+	//n个element方式赋值
+	deque<int> d3(10, 100);//100 100 100 100 100 100 100 100 100 100
+	PrintDeque(d3);
 }
 int main()
 {
