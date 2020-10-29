438f64b8258fccb0b5e622edb4e06030a4369119
Fowindy <47002339@qq.com>
2020-10-29 21:56:39 +0800

    改变map的默认排序为降序(仿函数)

--- Src/19_STL/47_map容器_排序.cpp
+++ Src/19_STL/47_map容器_排序.cpp
@@ -3,7 +3,16 @@
 #include <map>
 using namespace std;
 //47_map容器_排序
-void PrintMap47(const map<int, int>& m)
+class compareMap
+{
+public:
+	bool operator()(int v1, int v2)
+	{
+		//降序
+		return v1 > v2;
+	}
+};
+void PrintMap47(const map<int, int, compareMap>& m)
 {
 	for (map<int, int>::const_iterator it = m.begin(); it != m.end(); it++)
 	{
@@ -13,12 +22,13 @@ void PrintMap47(const map<int, int>& m)
 void test47()
 {
 	//map默认排序:按照key值升序
-	map<int, int>m;
+	map<int, int, compareMap>m;
 	m.insert(make_pair(1, 10));
 	m.insert(make_pair(2, 20));
 	m.insert(make_pair(5, 50));
 	m.insert(make_pair(3, 30));
 	m.insert(make_pair(4, 40));
+	//改变map的默认排序为降序(仿函数)
 	PrintMap47(m);
 }
 int main()
