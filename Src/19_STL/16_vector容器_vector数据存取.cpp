c28a83e3323eca6fff02069665b9e66c7aa8056a
Fowindy <47002339@qq.com>
2020-10-29 08:19:12 +0800

    获取第一个元素和获取最后一个元素

--- Src/19_STL/16_vector容器_vector数据存取.cpp
+++ Src/19_STL/16_vector容器_vector数据存取.cpp
@@ -23,6 +23,10 @@ void test16()
 		cout << v1.at(i) << " ";//0 1 2 3 4 5 6 7 8 9
 	}
 	cout << endl;
+	//获取第一个元素
+	cout << "v1的第一个元素为:" << v1.front() << endl;//v1的第一个元素为:0
+	//获取最后一个元素
+	cout << "v1的最后一个元素为:" << v1.back() << endl;//v1的最后一个元素为:9
 }
 int main()
 {
