6eec055638c351e60fef8a7a80ecdd6fedc5a846
Fowindy <47002339@qq.com>
2020-10-31 00:00:43 +0800

    管理员类框架完成

---
+++ Src/21_机房预约系统/Administrator.cpp
@@ -0,0 +1,46 @@
+﻿using namespace std;
+#include "Administrator.h"
+#include <iostream>
+#include <string>
+//构造函数
+Administrator::Administrator()
+{
+
+}
+
+Administrator::Administrator(string name, string pwd)
+{
+
+}
+
+void Administrator::addAccount()
+{
+
+}
+
+void Administrator::showAccount()
+{
+
+}
+
+void Administrator::showComputerRoom()
+{
+
+}
+
+void Administrator::cleanFile()
+{
+
+}
+
+//析构函数
+Administrator::~Administrator()
+{
+
+}
+
+void Administrator::operMenu()
+{
+
+}
+
---
+++ Src/21_机房预约系统/Administrator.h
@@ -0,0 +1,29 @@
+﻿#pragma once	//防止头文件重复包含
+#include <iostream>	//包含输入输出流的头文件
+#include "Identity.h"
+using namespace std;	//使用标准命名空间
+
+class Administrator :public Identity
+{
+public:
+	Administrator();	//构造函数
+	~Administrator();	//析构函数
+
+	//菜单界面
+	virtual void operMenu() override;
+
+	//有参构造
+	Administrator(string name, string pwd);
+
+	//添加账号
+	void addAccount();
+
+	//查看账号
+	void showAccount();
+
+	//查看机房信息
+	void showComputerRoom();
+
+	//清空预约记录
+	void cleanFile();
+};
