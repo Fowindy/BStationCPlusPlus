653a0062de10856fb13ecd6312c885323c9849aa
Fowindy <47002339@qq.com>
2020-10-30 13:47:49 +0800

    68_常用拷贝替换算法_replace

--- Src/19_STL/67_常用拷贝替换算法_copy.cpp
+++ Src/19_STL/67_常用拷贝替换算法_copy.cpp
@@ -26,7 +26,7 @@ void test67()
 	for_each(v1.begin(), v1.end(), MyPrint());//0 1 2 3 4 5 6 7 8 9
 	cout << endl;
 }
-int main()
+int main67()
 {
 	test67();
 	system("pause");
---
+++ Src/19_STL/68_常用拷贝替换算法_replace.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//68_常用拷贝替换算法_replace
+class MyPrint68
+{
+public:
+	void operator()(int val)
+	{
+		cout << val << " ";
+	}
+};
+void test68()
+{
+	vector<int>v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(20);
+	v.push_back(50);
+	v.push_back(10);
+	v.push_back(20);
+	cout << "------------------替换前:----------------" << endl;
+	for_each(v.begin(), v.end(), MyPrint68());//10 20 30 20 50 10 20
+	cout << endl;
+	cout << "------------------替换后:----------------" << endl;
+	replace(v.begin(), v.end(), 20, 999);
+	for_each(v.begin(), v.end(), MyPrint68());//10 999 30 999 50 10 999
+	cout << endl;
+}
+int main()
+{
+	test68();
+	system("pause");
+	return 0;
+}
