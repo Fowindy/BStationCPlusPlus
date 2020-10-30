999ca940ad203b0a9d5a902f367dd1913eb5e158
Fowindy <47002339@qq.com>
2020-10-30 15:27:17 +0800

    用户选择界面完成

--- Src/20_基于STL的演讲比赛流程管理系统/演讲比赛流程管理系统.cpp
+++ Src/20_基于STL的演讲比赛流程管理系统/演讲比赛流程管理系统.cpp
@@ -10,6 +10,28 @@ int main()
 	speechManager sm;
 	//显示菜单
 	sm.show_Menu();
+	//用于存储用户的输入
+	int choice = 0;
+	cout << "请输入您的选择:" << endl;
+	cin >> choice;
+	while (true)
+	{
+		sm.show_Menu();
+		switch (choice)
+		{
+		case 1://开始比赛
+			break;
+		case 2://查看往届比赛记录
+			break;
+		case 3://清空比赛记录
+			break;
+		case 0://退出系统
+			break;
+		default:
+			system("cls");//清屏
+			break;
+		}
+	}
 	system("pause");
 	return 0;
 }
