013cb33e674504b2078d15ab8c476d88753f1659
Fowindy <47002339@qq.com>
2020-10-30 23:15:26 +0800

    Identity头文件完成

---
+++ Src/21_机房预约系统/Identity.cpp
@@ -0,0 +1,16 @@
+﻿using namespace std;
+#include "Identity.h"
+#include <iostream>
+#include <string>
+//构造函数
+Identity::Identity()
+{
+
+}
+//析构函数
+Identity::~Identity()
+{
+
+}
+
+
---
+++ Src/21_机房预约系统/Identity.h
@@ -0,0 +1,19 @@
+﻿#pragma once	//防止头文件重复包含
+#include <iostream>	//包含输入输出流的头文件
+using namespace std;	//使用标准命名空间
+
+class Identity
+{
+public:
+	Identity();	//构造函数
+#pragma region 成员属性
+	string m_Name;//姓名
+	string m_Pwd;//密码
+#pragma endregion
+#pragma region 成员函数
+	//操作菜单纯虚函数
+	virtual void operMenu() = 0;
+#pragma endregion
+
+	~Identity();	//析构函数
+};
