54132ad7eebfded5eb4f9616c3baf2e8b5e0b110
Fowindy <47002339@qq.com>
2020-10-30 11:11:25 +0800

    56_常用遍历算法_transform

--- Src/19_STL/55_常用遍历算法_for_each.cpp
+++ Src/19_STL/55_常用遍历算法_for_each.cpp
@@ -31,7 +31,7 @@ void test55()
 	for_each(v.begin(), v.end(), print02());//0 1 2 3 4 5 6 7 8 9
 	cout << endl;
 }
-int main()
+int main55()
 {
 	test55();
 	system("pause");
---
+++ Src/19_STL/56_常用遍历算法_transform.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//56_常用遍历算法_transform
+//打印仿函数
+class Print
+{
+public:
+	void operator()(int val)
+	{
+		cout << val << " ";
+	}
+};
+//转换仿函数
+class Transform
+{
+public:
+	int operator()(int val)
+	{
+		return val + 100;
+	}
+};
+void test56()
+{
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	cout << "------------------未搬运前打印原容器:----------------" << endl;
+	for_each(v.begin(), v.end(), Print());//0 1 2 3 4 5 6 7 8 9
+	cout << endl;
+	//创建目标容器
+	vector<int>vTarget;
+	//为目标容器分配原容器一样的空间
+	vTarget.resize(v.size());
+	//转换
+	transform(v.begin(), v.end(), vTarget.begin(), Transform());
+	//打印转换后的目标容器
+	cout << "------------------打印转换后的目标容器----------------" << endl;
+	for_each(vTarget.begin(), vTarget.end(), Print());//100 101 102 103 104 105 106 107 108 109
+	cout << endl;
+}
+int main()
+{
+	test56();
+	system("pause");
+	return 0;
+}
