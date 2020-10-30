956a7237468a29360d2cba434cd2be9396a117d3
Fowindy <47002339@qq.com>
2020-10-31 00:08:46 +0800

    全局文件添加

---
+++ Src/21_机房预约系统/GlobalFile.h
@@ -0,0 +1,12 @@
+﻿#pragma once	//防止头文件重复包含
+
+//管理员文件
+#define ADMIN_FILE "admin.txt"
+//学生文件
+#define STUDENT_FILE "student.txt"
+//教师文件
+#define TEACHER_FILE "teacher.txt"
+//机房文件
+#define	COMPUTER_FILE "computerRoom.txt"
+//订单文件
+#define ORDER_FILE "order.txt"
