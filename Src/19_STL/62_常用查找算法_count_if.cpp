e31d3a76ee99704ef109dbc16e9c918460db31e7
Fowindy <47002339@qq.com>
2020-10-30 12:55:22 +0800

    2.统计自定义数据类型

--- Src/19_STL/62_常用查找算法_count_if.cpp
+++ Src/19_STL/62_常用查找算法_count_if.cpp
@@ -29,14 +29,49 @@ void test62()
 	cout << "大于等于30的数个数为:" << num << endl;
 	//大于等于30的数个数为:5
 }
+//2.统计自定义数据类型
+class Person62
+{
+public:
+	Person62(string name, int age)
+	{
+		this->m_Name = name;
+		this->m_Age = age;
+	}
+	string m_Name;
+	int m_Age;
+};
+//定义自定义查找谓词
+class CountPerson
+{
+public:
+	bool operator()(Person62& p)
+	{
+		return p.m_Age < 35;//小于35岁
+	}
+};
 void test062()
 {
-
+	vector<Person62>v;
+	Person62 p1("刘备", 35);
+	Person62 p2("关羽", 35);
+	Person62 p3("张飞", 35);
+	Person62 p4("赵云", 30);
+	Person62 p5("曹操", 25);
+	//将对象插入到容器中
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
+	v.push_back(p5);
+	//查找
+	int num = count_if(v.begin(), v.end(), CountPerson());
+	cout << "小于35岁的人个数为:" << num << endl;//小于35岁的人个数为:2
 }
-//2.统计自定义数据类型
 int main()
 {
 	test62();
+	test062();
 	system("pause");
 	return 0;
 }
