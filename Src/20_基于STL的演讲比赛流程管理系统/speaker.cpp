77fed5968ebb6323faca410f243e8cc7477ec6ba
Fowindy <47002339@qq.com>
2020-10-30 15:45:11 +0800

    选手类的创建

---
+++ Src/20_基于STL的演讲比赛流程管理系统/speaker.cpp
@@ -0,0 +1,15 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+#include "speaker.h"
+//构造函数
+speaker::speaker()
+{
+}
+
+//析构函数
+speaker::~speaker()
+{
+}
+
+
---
+++ Src/20_基于STL的演讲比赛流程管理系统/speaker.h
@@ -0,0 +1,12 @@
+﻿#pragma once	//防止头文件重复包含
+#include <iostream>	//包含输入输出流的头文件
+using namespace std;	//使用标准命名空间
+
+class speaker
+{
+public:
+	speaker();	//构造函数
+	string m_Name;//姓名
+	double m_Score[2];//分数 最多有两轮的得分
+	~speaker();	//析构函数
+};
