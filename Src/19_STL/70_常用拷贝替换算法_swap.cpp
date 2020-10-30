64822b9e52a3793646849e719c20b83d40bbcf9c
Fowindy <47002339@qq.com>
2020-10-30 14:07:17 +0800

    71_常用算术生成算法_accumulate

--- Src/19_STL/70_常用拷贝替换算法_swap.cpp
+++ Src/19_STL/70_常用拷贝替换算法_swap.cpp
@@ -33,7 +33,7 @@ void test70()
 	for_each(v2.begin(), v2.end(), MyPrint70());//0 1 2 3 4 5 6 7 8 9
 	cout << endl;
 }
-int main()
+int main70()
 {
 	test70();
 	system("pause");
---
+++ Src/19_STL/71_常用算术生成算法_accumulate.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+//71_常用算术生成算法_accumulate
+void test71()
+{
+	vector<int>v;
+	for (int i = 0; i <= 100; i++)
+	{
+		v.push_back(i);
+	}
+	//参数3:起始累加值,如不需要写0就好
+	int total = accumulate(v.begin(), v.end(), 1000);
+	cout << "total = " << total << endl;//total = 6050
+}
+int main()
+{
+	test71();
+	system("pause");
+	return 0;
+}
