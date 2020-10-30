48c1ead4f78f53735a140095028d3975022f22b6
Fowindy <47002339@qq.com>
2020-10-30 10:57:26 +0800

    遍历算法_仿函数_传入匿名对象

--- Src/19_STL/55_常用遍历算法_for_each.cpp
+++ Src/19_STL/55_常用遍历算法_for_each.cpp
@@ -8,6 +8,14 @@ void print01(int val)
 {
 	cout << val << " ";
 }
+class print02
+{
+public:
+	void operator()(int val)
+	{
+		cout << val << " ";
+	}
+};
 void test55()
 {
 	vector<int>v;
@@ -18,6 +26,9 @@ void test55()
 	}
 	//遍历算法_普通函数_传入函数名即可
 	for_each(v.begin(), v.end(), print01);//0 1 2 3 4 5 6 7 8 9
+	cout << endl;
+	//遍历算法_仿函数_传入匿名对象
+	for_each(v.begin(), v.end(), print02());//0 1 2 3 4 5 6 7 8 9
 	cout << endl;
 }
 int main()
