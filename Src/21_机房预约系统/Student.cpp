9a3fe97adc608e1a6f0160183263a15363b2b553
Fowindy <47002339@qq.com>
2020-10-30 23:34:30 +0800

    学生类框架完成

---
+++ Src/21_机房预约系统/Student.cpp
@@ -0,0 +1,53 @@
+﻿using namespace std;
+#include "Student.h"
+#include <iostream>
+#include <string>
+//构造函数
+Student::Student()
+{
+
+}
+
+//有参构造 参数:学号,姓名,密码
+Student::Student(int id, string name, string pwd)
+{
+
+}
+
+//申请预约
+void Student::applyOrder()
+{
+
+}
+
+//查看我的预约
+void Student::showMyOrder()
+{
+
+}
+
+//查看所有预约
+void Student::showAllOrder()
+{
+
+}
+
+//取消预约
+void Student::cancelOrder()
+{
+
+}
+
+//注销登录
+void Student::signOut()
+{
+
+}
+
+//析构函数
+Student::~Student()
+{
+
+}
+
+
---
+++ Src/21_机房预约系统/Student.h
@@ -0,0 +1,39 @@
+﻿#pragma once	//防止头文件重复包含
+#include <iostream>	//包含输入输出流的头文件
+#include "Identity.h"
+using namespace std;	//使用标准命名空间
+
+class Student :public Identity
+{
+public:
+#pragma region 成员函数
+	//构造函数(默认构造)
+	Student();
+
+	//有参构造 参数:学号,姓名,密码
+	Student(int id, string name, string pwd);
+
+	//申请预约
+	void applyOrder();
+
+	//查看我的预约
+	void showMyOrder();
+
+	//查看所有预约
+	void showAllOrder();
+
+	//取消预约
+	void cancelOrder();
+
+	//注销登录
+	void signOut();
+
+	//析构函数
+	~Student();
+#pragma endregion
+
+#pragma region 成员属性
+
+#pragma endregion
+
+};
