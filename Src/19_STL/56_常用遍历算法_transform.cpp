67a8721a514040d58fbcea70f6ccbae1cc134fcc
Fowindy <47002339@qq.com>
2020-10-30 11:24:41 +0800

    1.查找 内置数据类型

--- Src/19_STL/56_常用遍历算法_transform.cpp
+++ Src/19_STL/56_常用遍历算法_transform.cpp
@@ -43,7 +43,7 @@ void test56()
 	for_each(vTarget.begin(), vTarget.end(), Print());//100 101 102 103 104 105 106 107 108 109
 	cout << endl;
 }
-int main()
+int main56()
 {
 	test56();
 	system("pause");
---
+++ Src/19_STL/57_常用查找算法_find.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//57_常用查找算法_find
+//1.查找 内置数据类型
+void test57()
+{
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	vector<int>::iterator it = find(v.begin(), v.end(), 5);
+	if (it != v.end())
+	{
+		cout << "找到元素:" << *it << endl;//找到元素:5
+	}
+	else
+	{
+		cout << "未找到元素!" << endl;
+	}
+}
+int main()
+{
+	test57();
+	system("pause");
+	return 0;
+}
